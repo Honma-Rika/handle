b6ae41590f9f89ef708fa21ac52f04049b726219
Honmma <erolspiegel@gmail.com>
2022-03-13 21:10:53 +0800

    absolute path

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
       '~/': `${path.resolve(__dirname, 'src')}/`,
     },
   },
-  base: '/',
+  base: 'honma-rika.github.io/handle/',
   plugins: [
     Vue(),
     AutoImport({
